2b1cd58798ae568901c5064f695076a27c2a5ffc
Rupam <rb.ccsu@gmail.com>
2017-02-21 19:24:38 +0530

    Create VideoProcessingModule.java

---
+++ Modules/src/VideoProcessingModule.java
@@ -0,0 +1,7 @@
+/*
+ * @author Rupam Bhattacharyya
+ * Two core functionalities related to human joint and object has been done.
+ * Annotations from CAD-120 Video dataset is going to be utilized here.
+ * skeletonManager() and calculateCentroid() are two core functions. 
+ * Started on 29-11-2016 
+*/
