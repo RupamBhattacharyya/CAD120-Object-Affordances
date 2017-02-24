ccec6308a04c16b18ff7415b9af3141e8279cfb7
Rupam <rb.ccsu@gmail.com>
2017-02-23 22:06:56 -0800

    Update MasterMain.java

--- Modules/src/MasterMain.java
+++ Modules/src/MasterMain.java
@@ -2,7 +2,6 @@
 /**
  * @author Rupam Bhattacharyya
  * This project has 4 classes VideoProcessingModule.java, OntologyProcessingModule.java, DeductionModule.java and DecisionModule.java 
- * This main file(MasterMain.java) is created for Algorithm 4 of the Paper.
  * It is able to produce affordance result presented in the paper.
  * Started on November 29th, 2016.
 */
